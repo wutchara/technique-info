675533cfac7846d886ffd00d03592478f223d5cd
wutchara <SSTMUHAM@gmail.com>
2023-10-06 15:46:29 +0700

    Update deyect-objects.py

--- basic/image-processing/image-detections/deyect-objects.py
+++ basic/image-processing/image-detections/deyect-objects.py
@@ -16,7 +16,7 @@ def showImage(img):
 # Convert to grey sacle
 # =========================
 
-original_image = imread('img.png')
+original_image = imread('candies.jpeg')
 showImage(original_image)
 
 im = rgb2gray(original_image) # grayscale image
@@ -29,32 +29,34 @@ showImage(im_bw)
 # =========================
 # Theshole
 # =========================
-# def dilate(im,num):
-#     """
-#     Performs dilation morphology num times.
-#     """
-#     for i in range(num):
-#         im = dilation(im)
-#     return im
+def dilate(im,num):
+    """
+    Performs dilation morphology num times.
+    """
+    for i in range(num):
+        im = dilation(im)
+    return im
 
-# def area_close(im, num):
-#     """
-#     Performs area_closing morphology num times.
-#     """
-#     for i in range(num):
-#         im = area_closing(im)
-#     return im
+def area_close(im, num):
+    """
+    Performs area_closing morphology num times.
+    """
+    for i in range(num):
+        im = area_closing(im)
+    return im
 
-# im_cleaned = dilate(im_bw,5)
-# im_cleaned = area_close(im_cleaned, 40)
-# im_cleaned = median_filter(im_cleaned, size=21)
+im_cleaned = dilate(im_bw, 5)
 # showImage(im_cleaned)
+im_cleaned = area_close(im_cleaned, 40)
+# showImage(im_cleaned)
+im_cleaned = median_filter(im_cleaned, size=21)
+showImage(im_cleaned)
 
 # =========================
 # Detect objects
 # =========================
 
-# LoG => Laplacian of Gaussian
+# # LoG => Laplacian of Gaussian
 # blobs_log = blob_log(im_bw, max_sigma=30, num_sigma=10, threshold=.1)
 # blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)# Compute radii in the 3rd column.
 # showImage(blobs_log)
@@ -71,18 +73,19 @@ showImage(im_bw)
 # =========================
 # Connect components
 # =========================
-# img_label = label(im_cleaned) # label objects
-# regions = regionprops(img_label) # get region properties
+img_label = label(im_cleaned) # label objects
+regions = regionprops(img_label) # get region properties
 
-# plt.figure(figsize=(12,5))
+plt.figure(figsize=(12,5))
 
-# # create bounding box
-# for props in regions:
-#     minr, minc, maxr, maxc = props.bbox
-#     bx = (minc, maxc, maxc, minc, minc)
-#     by = (minr, minr, maxr, maxr, minr)
-#     plt.plot(bx, by, '-r', linewidth=0.5)
+# create bounding box
+for props in regions:
+    minr, minc, maxr, maxc = props.bbox
+    bx = (minc, maxc, maxc, minc, minc)
+    by = (minr, minr, maxr, maxr, minr)
+    plt.plot(bx, by, '-r', linewidth=0.5)
 
-# plt.imshow(img_label)
-# plt.axis('off')
-# plt.title('Detected Objects\nCount: {}'.format(len(regions)))
+plt.imshow(img_label)
+plt.axis('off')
+plt.title('Detected Objects\nCount: {}'.format(len(regions)))
+showImage(img_label)
