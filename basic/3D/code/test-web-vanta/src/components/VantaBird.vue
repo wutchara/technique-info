ec5b2edce85463cfd0da541e78d2f20e3079b236
wutchara <sstmuham@gmail.com>
2023-11-16 21:29:14 +0700

    add example for 3d vanta lib

---
+++ basic/3D/code/test-web-vanta/src/components/VantaBird.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="vantaRef">Foreground content here</div>
+</template>
+
+<script>
+import BIRDS from "vanta/dist/vanta.birds.min";
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+export default {
+  mounted() {
+    this.vantaEffect = BIRDS({
+      el: this.$refs.vantaRef,
+    });
+  },
+  beforeUnmount() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy();
+    }
+  },
+};
+</script>
